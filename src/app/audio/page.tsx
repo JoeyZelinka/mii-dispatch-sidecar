e7c56a629cb9309d10ed65d11cb2404309f23a56
Joey Zelinka <cheflife365@gmail.com>
2026-07-20 12:38:09 -0400

    renetlify

--- src/app/audio/page.tsx
+++ src/app/audio/page.tsx
@@ -1,4 +1,8 @@
-import AudioClient from './AudioClient';
+'use client';
+
+import dynamic from 'next/dynamic';
+
+const AudioClient = dynamic(() => import('./AudioClient'), { ssr: false });
 
 export default function Page() {
   return <AudioClient />;
